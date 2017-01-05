eaf91e42476e138df984d9967960b4c03eb59205
Matthias Fischmann <mf@zerobuzz.net>
2017-01-05 19:25:19 +0100

    Write simple, preliminary script for ci testing.

---
+++ test-all.hs
@@ -0,0 +1,42 @@
+#!/usr/bin/env stack
+{- stack --resolver lts-7.14 --install-ghc runghc
+    --package directory
+    --package turtle
+
+    --
+
+    -XBangPatterns
+    -XOverloadedStrings
+    -XScopedTypeVariables
+    -XTupleSections
+    -XViewPatterns
+
+    -Wall
+
+-}
+
+import           Data.Monoid
+import           System.Directory
+import           System.Exit
+import           System.Process
+
+-- TODO: we need to use something that does better reporting while executing.  shake, probably?
+
+packages :: [FilePath]
+packages = ["refine-prelude", "refine-common", "refine-backend", "refine-frontend"]
+
+main :: IO ()
+main = do
+  (\fp -> setupPkg fp >> testPkg fp) `mapM_` packages
+
+setupPkg :: FilePath -> IO ()
+setupPkg target = withCurrentDirectory target $ do
+  putStrLn $ "\n\n>>> " <> target <> " [setup]\n\n"
+  ExitSuccess <- system "stack setup && stack install --fast --test --dependencies-only"  -- FUTUREWORK: --coverage --bench
+  pure ()
+
+testPkg :: FilePath -> IO ()
+testPkg target = withCurrentDirectory target $ do
+  putStrLn $ "\n\n>>> " <> target <> " [test]\n\n"
+  ExitSuccess <- system $ "stack test --fast " <> show target
+  pure ()
