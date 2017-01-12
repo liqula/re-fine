e5574af952eebe97ca04729179914fefbcfa6386
Matthias Fischmann <mf@zerobuzz.net>
2017-01-12 18:31:10 +0100

    Test suite module structure.
    keep test modules corresponding to production modules separate
    from helper modules and / general test modules.

--- refine-common/test/Arbitrary.hs
+++ refine-common/test/Arbitrary.hs
@@ -1,11 +1,10 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Refine.Common.Types.Arbitrary where
+module Arbitrary where
 
 import Test.QuickCheck
 
 import Refine.Common.Types.Prelude
-
 
 
 instance Arbitrary (ID a) where
