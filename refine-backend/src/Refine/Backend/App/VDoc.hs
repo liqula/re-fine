7b3a65efa70d246f6adaa9c41d90e8837b68c998
Matthias Fischmann <mf@zerobuzz.net>
2017-01-14 09:15:48 +0100

    Backend rest-level test suite (failing).

--- refine-backend/src/Refine/Backend/App/VDoc.hs
+++ refine-backend/src/Refine/Backend/App/VDoc.hs
@@ -28,9 +28,13 @@ import           Refine.Backend.App.Core
 import           Refine.Backend.Database (DB)
 import           Refine.Backend.Database.Entity as DB
 import qualified Refine.Backend.DocRepo as DocRepo
+import           Refine.Common.Rest (HeavyVDoc)
 import           Refine.Common.Types.Prelude
 import           Refine.Common.Types.VDoc
 
+
+listVDocs :: App DB [ID VDoc]
+listVDocs = undefined
 
 createVDoc :: Create VDoc -> App DB VDoc
 createVDoc pv = do
@@ -48,6 +52,9 @@ getVDoc :: ID VDoc -> App DB VDoc
 getVDoc i = do
   appLog "getVDoc"
   db $ loadVDoc i
+
+getHeavyVDoc :: ID VDoc -> App DB HeavyVDoc
+getHeavyVDoc = undefined
 
 getVersion :: ID Patch -> App DB (VDocVersion 'HTMLWithMarks)
 getVersion _ = do
