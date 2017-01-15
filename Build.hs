3edbeafb8d1d1b4ead20e2737252489884509169
Matthias Fischmann <mf@zerobuzz.net>
2017-01-15 17:55:18 +0100

    eliminate rule string names.
    i think this extra level of indirection is confusing to the script
    *user* (not developer).  i usually just want to see what rules there
    are, and with the haskell identifiers i have to then make an extra
    jump and look up what they are called on the command line.

--- Build.hs
+++ Build.hs
@@ -31,51 +31,36 @@ hlintProject project = do
     ]
 
 
--- * commands
-
-setup, buildPrelude, buildCommon, buildBackend, buildFrontend, buildAll, clean, distClean, hlint :: String
-
-setup         = "setup"
-buildPrelude  = "build-prelude"
-buildCommon   = "build-common"
-buildBackend  = "build-backend"
-buildFrontend = "build-frontend"
-buildAll      = "build-all"
-clean         = "clean"
-distClean     = "dist-clean"
-hlint         = "hlint"
-
-
 -- * main
 
 main :: IO ()
 main = shakeArgs shakeOptions { shakeFiles = ".build", shakeVerbosity = Loud } $ do
 
   want
-    [ buildAll
-    , hlint
+    [ "build-all"
+    , "hlint"
     ]
 
-  phony setup $ do
+  phony "setup" $ do
     let resolver = "nightly-2017-01-10"  -- (we need at least hlint v1.9.39, which is not in lts-7.15.)
     command_ [] "stack" ["install", "hlint", "--resolver", resolver]
     command_ [] "stack" ["exec", "--", "hlint", "--version"]
     command_ [] "stack" ["install", "hspec-discover", "--resolver", resolver]
     command_ [] "stack" ["exec", "--", "which", "hspec-discover"]
 
-  phony buildPrelude $ do
+  phony "build-prelude" $ do
     stackBuild refinePrelude
 
-  phony buildCommon $ do
+  phony "build-common" $ do
     stackBuild refineCommon
 
-  phony buildBackend $ do
+  phony "build-backend" $ do
     stackBuild refineBackend
 
-  phony buildFrontend $ do
+  phony "build-frontend" $ do
     stackBuild refineFrontend
 
-  phony buildAll $ do
+  phony "build-all" $ do
     -- for building everything, we only need to go to backend and frontend.  prelude and common are
     -- compiled in both (two different compilers), and tested (as non-extra deps in stack.yaml) in
     -- backend.
@@ -85,15 +70,15 @@ main = shakeArgs shakeOptions { shakeFiles = ".build", shakeVerbosity = Loud } $
     stackBuild refineBackend
     stackBuild refineFrontend
 
-  phony clean $ do
+  phony "clean" $ do
     forM_ [refinePrelude, refineCommon, refineBackend, refineFrontend] $ \pkg -> do
         command_ [Cwd pkg] "stack" ["clean"]
 
-  phony distClean $ do
+  phony "dist-clean" $ do
     forM_ [refinePrelude, refineCommon, refineBackend, refineFrontend] $ \pkg -> do
         command_ [Cwd pkg] "rm" ["-rf", ".stack-work"]
 
-  phony hlint $ do
+  phony "hlint" $ do
     hlintProject refinePrelude
     hlintProject refineCommon
     hlintProject refineBackend
