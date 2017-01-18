188b7d6aa71857e4bf6c3cfce6de56454a38b2c2
Matthias Fischmann <mf@zerobuzz.net>
2017-01-18 12:21:44 +0100

    Make build.sh callable from any directory.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+set -e
+cd `dirname $0`
 mkdir -p .shake
 stack exec -- ghc -j --make Build.hs -threaded -rtsopts -with-rtsopts="-N" -outputdir=.shake -o .shake/build && .shake/build +RTS -N -RTS "$@"
