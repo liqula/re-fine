c49178b06a4c2a0ad9c1eab867e62fd81980838f
Matthias Fischmann <mf@zerobuzz.net>
2017-01-20 23:07:31 +0100

    Refactor: move re-usable code from HttpApiSpec to Refine.CommonTest.*.

--- refine-common/src/Refine/Common/Test/HttpApiData.hs
+++ refine-common/src/Refine/Common/Test/HttpApiData.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE GADTs                     #-}
 
-module HttpApiSpec where
+module Refine.Common.Test.HttpApiData where
 
 import Data.String.Conversions (cs)
 import Data.Typeable (Typeable, typeOf)
@@ -9,7 +9,6 @@ import Test.QuickCheck
 import Web.HttpApiData
 
 import Refine.Common.Test.Arbitrary ()
-import Refine.Common.Types.Prelude
 
 
 data HttpApiGen d where
@@ -23,10 +22,3 @@ fromAndToHttpApiDataAreInverses :: HttpApiGen d -> Spec
 fromAndToHttpApiDataAreInverses (HttpApiGen eq g s) =
     it (show $ typeOf g) . property . forAllShrink g s $ \httpApiData ->
         either (error . cs) (eq httpApiData) . parseUrlPiece $ toUrlPiece httpApiData
-
-spec :: Spec
-spec = parallel $ do
-  describe "FromHttpApiData and ToHttpApiData are inverses" .
-    mapM_ fromAndToHttpApiDataAreInverses $
-      [ h :: HttpApiGen (ID ())
-      ]
