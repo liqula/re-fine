e9362a6cc38376369bf8fd608ba3ee71f47cfc16
Matthias Fischmann <mf@zerobuzz.net>
2017-01-24 17:24:53 +0100

    Introduce FileTree type, fill out HtmlFileTreeSpec module.

--- refine-backend/src/Refine/Backend/DocRepo/HtmlFileTree.hs
+++ refine-backend/src/Refine/Backend/DocRepo/HtmlFileTree.hs
@@ -20,14 +20,39 @@
 
 module Refine.Backend.DocRepo.HtmlFileTree where
 
+import Data.String.Conversions
+import System.IO.Temp (createTempDirectory)
+
 import Refine.Common.Types.VDoc
+
+
+-- | Compose 'htmlToFileTree', 'writeFileTree'.
+writeHtml :: FilePath -> VDocVersion 'HTMLCanonical -> IO FilePath
+writeHtml rootPath vers = do
+  repoPath <- createTempDirectory rootPath ".repo"
+  writeFileTree repoPath $ htmlToFileTree vers
+  pure repoPath
+
+-- | Compose 'readFileTree', 'htmlFromFileTree'.
+readHtml :: FilePath -> IO (VDocVersion 'HTMLCanonical)
+readHtml = fmap htmlFromFileTree . readFileTree
 
 
 -- | Take a file path 'fp' and a canonicalized vdoc version, create a directory under 'fp', unravel
 -- the vdoc version into that directory and return its filepath.
-htmlToFileTree :: FilePath -> VDocVersion 'HTMLCanonical -> IO FilePath
+htmlToFileTree :: VDocVersion 'HTMLCanonical -> FileTree
 htmlToFileTree = undefined
 
 -- | Take the file path of an unravelled vdoc version, re-ravel it and return it.
-htmlFromFileTree :: FilePath -> IO (VDocVersion 'HTMLCanonical)
+htmlFromFileTree :: FileTree -> VDocVersion 'HTMLCanonical
 htmlFromFileTree = undefined
+
+
+data FileTree = File FilePath ST | Directory FilePath [FileTree]
+  deriving (Eq, Show)
+
+writeFileTree :: FilePath -> FileTree -> IO ()
+writeFileTree _rootPath _ft = undefined
+
+readFileTree :: FilePath -> IO FileTree
+readFileTree _rootPath = undefined
--- refine-backend/test/Refine/Backend/DocRepo/HtmlFileTreeSpec.hs
+++ refine-backend/test/Refine/Backend/DocRepo/HtmlFileTreeSpec.hs
@@ -18,20 +18,94 @@
 {-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE ViewPatterns               #-}
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Refine.Backend.DocRepo.HtmlFileTreeSpec where
 
+import Data.String.Conversions
+import Data.Text.IO as ST
+import System.Directory
 import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
+
+import Refine.Backend.DocRepo.HtmlFileTree
+import Refine.Backend.Test.Util (withTempCurrentDirectory)
+import Refine.Common.Test.Arbitrary ()
+import Refine.Common.Types
+
+
+instance Arbitrary FileTree where
+  arbitrary = oneof
+    [ File <$> arbitrary <*> arbitrary
+    , Directory <$> arbitrary <*> scale (`div` 2) arbitrary
+    ]
+
+  shrink (File fp c) = File <$> shrink fp <*> shrink c
+  shrink (Directory dp c) = Directory <$> shrink dp <*> shrink c
 
 
 spec :: Spec
-spec = describe "htmlToFileTree" $ do
-  describe "unit tests" $ do
-    it "creates files for text nodes." pending
-    it "creates files for comment nodes." pending
-    it "creates directories for tag nodes." pending
-    it "stores tag names in file TAGNAME." pending
-    it "stores attribute key-value pairs in file ATTRS." pending
+spec = do
+  describe "htmlToFileTree" $ do
+    it "creates files for text nodes." $
+      htmlToFileTree (VDocVersion "phoo")
+        `shouldBe` File "00001" "phoo"
 
-  describe "qc properties" $ do
-    it "is inverse of htmlFromFileTree." pending
-    it "creates a directory for every tag open/close element pair." pending
+    it "creates files for comment nodes." $
+      htmlToFileTree (VDocVersion "<!-- phoo -->")
+        `shouldBe` File "00001" "<!-- phoo -->"
+
+    it "creates directories for tag nodes and stores tag names in file TAGNAME." $
+      htmlToFileTree (VDocVersion "<div></div>")
+        `shouldBe` Directory "00001" [File "TAGNAME" "div"]
+
+    it "stores attribute key-value pairs in file ATTRS." $
+      htmlToFileTree (VDocVersion "<div a=\"3\" bw=\"phoo\"></div>")
+        `shouldBe` Directory "00001" [File "ATTRS" "a=\"3\"\nbw=\"phoo\"", File "TAGNAME" "div"]
+
+    it "is inverse of htmlFromFileTree." . property $ \vers ->
+      htmlFromFileTree (htmlToFileTree vers) `shouldBe` vers
+
+  describe "writeHtml, readHtml" $ do
+    it "are inverses." $ do
+      -- (we've covered property checking of html to/from file tree above.  but we should run these
+      -- two functions once anyway, just to make sure.)
+      let vers = VDocVersion "<div a=\"3\" bw=\"phoo\">whee<p>bla</p>furgh<span>3</span></div>"
+      withTempCurrentDirectory (writeHtml "." vers >>= readHtml)
+        `shouldReturn` vers
+
+  describe "readFileTree" $ do
+    it "reads top-level file correctly." $ do
+      fileTree <- withTempCurrentDirectory $ do
+        ST.writeFile "name" "contents"
+        readFileTree =<< getCurrentDirectory
+      fileTree `shouldBe` (File "name" "contents")
+
+    it "reads directory correctly." $ do
+      fileTree <- withTempCurrentDirectory $ do
+        createDirectory "name"
+        readFileTree =<< getCurrentDirectory
+      fileTree `shouldBe` (Directory "name" [])
+
+    it "reads nested file correctly." $ do
+      fileTree <- withTempCurrentDirectory $ do
+        createDirectory "dir"
+        ST.writeFile "./dir/file" "content"
+        readFileTree "."
+      fileTree `shouldBe` (Directory "dir" [File "file" "content"])
+
+    it "reads multiple files in lexicographical order." $ do
+      fileTree <- withTempCurrentDirectory $ do
+        createDirectory "dir"
+        ST.writeFile "./dir/a" "a"
+        ST.writeFile "./dir/c" "c"
+        ST.writeFile "./dir/b" "b"
+        ST.writeFile "./dir/2" "2"
+        ST.writeFile "./dir/10" "10"
+        readFileTree =<< getCurrentDirectory
+      fileTree `shouldBe` (Directory "dir" ((\s -> File (cs s) s) <$> ["10", "2", "a", "b", "c"]))
+
+  describe "writeFileTree" $ do
+    it "is inverse of readFileTree." . property $ \ft ->
+      withTempCurrentDirectory (writeFileTree "." ft >> readFileTree ".") `shouldReturn` ft
