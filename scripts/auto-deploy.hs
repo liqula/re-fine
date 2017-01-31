385141c6d450a75333fe2984fd02842883f3defe
Matthias Fischmann <mf@zerobuzz.net>
2017-01-31 14:11:53 +0100

    Simplify deploy scripts.

--- scripts/auto-deploy.hs
+++ scripts/auto-deploy.hs
@@ -3,6 +3,7 @@
     --package string-conversions
     --package wai
     --package warp
+    --package executable-path
 
     --
 
@@ -24,17 +25,26 @@ import Data.String.Conversions
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp
+import System.Directory
 import System.Environment
 import System.Process
+import System.Environment.Executable
+import System.FilePath
+
 
 main :: IO ()
 main = do
   [read -> port] <- getArgs
+  RunGHC ((</> "..") . takeDirectory -> workingDir) <- getScriptPath
+  putStrLn $ "setting working directory to " <> show workingDir
+  setCurrentDirectory workingDir
   putStrLn $ "running refresh trigger on port " <> show port
 
   mvar <- newMVar Nothing
   bump mvar
-  run port (\_req respond -> bump mvar >> respond (responseLBS status201 [] "refreshed!"))
+  let resp = responseLBS status201 [] "refreshing refine backend, please bear with us...\n"
+      app _req respond = bump mvar >> respond resp
+  run port app
 
 bump :: MVar (Maybe ProcessHandle) -> IO ()
 bump mvar = modifyMVar_ mvar $ fmap Just . restart
@@ -44,4 +54,8 @@ restart mpid = do
   (\pid -> terminateProcess pid >> waitForProcess pid) `mapM_` mpid
   callProcess "git" ["checkout", "master"]
   callProcess "git" ["pull"]
-  spawnProcess "./scripts/build-staging.sh" []
+  callProcess "./build" ["setup"]
+  callProcess "./build" ["build-backend"]
+  callProcess "./build" ["build-frontend"]
+  withCurrentDirectory "pkg/frontend" $ callProcess "npm" ["run", "build"]
+  withCurrentDirectory "pkg/backend"  $ spawnProcess "stack" ["exec", "--", "refine", "server.conf"]
