2c0a166cc24946a114be6f588ea4c7debfbd1013
Matthias Fischmann <mf@zerobuzz.net>
2017-02-03 12:23:21 +0100

    Add build info helpers to prelude.

---
+++ pkg/prelude/src/Refine/Prelude/BuildInfo/TH.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE ExplicitForAll             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeFamilyDependencies     #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE ViewPatterns               #-}
+
+-- | (This module is necessary due to GHC/TH stage restrictions.)
+module Refine.Prelude.BuildInfo.TH
+where
+
+import Data.Data (Data)
+import Language.Haskell.TH (Q, Exp, runIO)
+import Language.Haskell.TH.Quote (dataToExpQ)
+import System.Process (readProcess)
+import Data.Time (getCurrentTime, formatTime, defaultTimeLocale)
+
+
+gitCommitHashIO :: IO String
+gitCommitHashIO = take 8 <$> readProcess "git" ["rev-parse", "HEAD"] ""
+
+gitBuildTimestampIO :: IO String
+gitBuildTimestampIO = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S %Z" <$> getCurrentTime
+
+runAtbuildTime :: Data a => IO a -> Q Exp
+runAtbuildTime action = runIO action >>= dataToExpQ (const Nothing)
