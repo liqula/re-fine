5040d7c1c6e5fece754c3b079d658c87355f25ce
Matthias Fischmann <mf@zerobuzz.net>
2017-02-06 17:59:53 +0100

    Document DataUID, DataChunkID, OwnerKind.

--- pkg/common/src/Refine/Common/VDoc/HTML/Core.hs
+++ pkg/common/src/Refine/Common/VDoc/HTML/Core.hs
@@ -82,7 +82,13 @@ data ChunkRangeError =
 data PreToken = PreToken Token | PreMarkOpen DataChunkID OwnerKind | PreMarkClose DataChunkID
   deriving (Eq, Show, Generic)
 
+-- | 'ID' of 'Edit' or 'Comment' that owns a 'ChunkRange'.  This is the @data-chunk-id@ attribute value
+-- of the @mark@ tag corresponding to the 'ChunkRange' in the dom.
 type DataChunkID = ST  -- FIXME: @newtype DataChunkID (forall a . Eq a => ID a)@
+
+-- | This is the @data-chunk-owner@ attribute value of the @mark@ tag corresponding to the
+-- 'ChunkRange' in the dom.  It can be either @edit@, @note@, @question@, @discussion@, or something
+-- like that.
 type OwnerKind = ST    -- FIXME: @type OwnerKind = TypeRep  -- e.g. @ID Edit@@
 
 -- | Needed to make some of the helper functions total.
