cc8a5e9e956fc130068be18f40fd09a50c12174b
Matthias Fischmann <mf@zerobuzz.net>
2017-02-06 16:17:05 +0100

    Fix: consider left siblings in getRangeAt polishing.

--- pkg/frontend/static/hookup.js
+++ pkg/frontend/static/hookup.js
@@ -12,19 +12,31 @@ window.Hammer = require("react-hammerjs");
 require("./../scss/main.scss");
 
 
-var refine$getSelectionRange = function() {
+window.refine$getSelectionRange = function() {
+    var leftSiblingLength = function(node) {
+        if (node === null) {
+            return 0;
+        } else {
+            return (node.textContent || node.innerText).length + leftSiblingLength(node.previousElementSibling);
+        }
+    };
+
     return (function (range) {
         var result = {};
         if(range.startContainer.parentElement.attributes['data-uid']) {
+            var leftOffset = leftSiblingLength(range.startContainer);
+            var dataOffset = parseInt(range.startContainer.parentElement.attributes['data-offset'].value, 10);
             result.start = {
                 node: parseInt(range.startContainer.parentElement.attributes['data-uid'].value, 10),
-                offset: range.startOffset + parseInt(range.startContainer.parentElement.attributes['data-offset'].value, 10)
+                offset: range.startOffset + leftOffset + dataOffset
             };
         }
         if(range.endContainer.parentElement.attributes['data-uid']) {
+            var leftOffset = leftSiblingLength(range.endContainer);
+            var dataOffset = parseInt(range.endContainer.parentElement.attributes['data-offset'].value, 10);
             result.end = {
                 node: parseInt(range.endContainer.parentElement.attributes['data-uid'].value, 10),
-                offset: range.endOffset + parseInt(range.endContainer.parentElement.attributes['data-offset'].value, 10)
+                offset: range.endOffset + leftOffset + dataOffset
             };
         }
         result.top = range.startContainer.parentElement.getBoundingClientRect().top;
