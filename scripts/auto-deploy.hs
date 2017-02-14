059ca988af1ff8c37f439dd69096d8ca3ef9e2d7
Matthias Fischmann <mf@zerobuzz.net>
2017-02-14 11:57:03 +0100

    Refactor; --run-once flag for auto-deploy.hs.

--- scripts/auto-deploy.hs
+++ scripts/auto-deploy.hs
@@ -21,42 +21,62 @@
 -}
 
 import Control.Concurrent.MVar
+import Control.Exception
 import Data.String.Conversions
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp
 import System.Directory
 import System.Environment
-import System.Process
 import System.Environment.Executable
 import System.FilePath
+import System.Process
+import Text.Read
 
 
 main :: IO ()
 main = do
-  [read -> port] <- getArgs
+  setProperCurrentDirectory
+  args <- getArgs
+  case args of
+    [readMaybe . cs -> Just port] -> do
+      putStrLn $ "running upgrade trigger on port " <> show port
+      mvar <- newMVar Nothing
+      bump mvar
+      let resp = responseLBS status201 [] "server updated and upgraded!\n"
+          app _req respond = bump mvar >> respond resp
+      run port app
+
+    ["--run-once"] -> do
+      buildFast >>= waitForProcess >> pure ()
+
+    _ -> do
+      throwIO $ ErrorCall "bad arguments."
+
+setProperCurrentDirectory :: IO ()
+setProperCurrentDirectory = do
   RunGHC ((</> "..") . takeDirectory -> workingDir) <- getScriptPath
   putStrLn $ "setting working directory to " <> show workingDir
   setCurrentDirectory workingDir
-  putStrLn $ "running refresh trigger on port " <> show port
-
-  mvar <- newMVar Nothing
-  bump mvar
-  let resp = responseLBS status201 [] "server updated and restarted!\n"
-      app _req respond = bump mvar >> respond resp
-  run port app
 
 bump :: MVar (Maybe ProcessHandle) -> IO ()
-bump mvar = modifyMVar_ mvar $ fmap Just . restart
+bump mvar = modifyMVar_ mvar $ fmap Just . upgrade
 
-restart :: Maybe ProcessHandle -> IO ProcessHandle
-restart mpid = do
+upgrade :: Maybe ProcessHandle -> IO ProcessHandle
+upgrade mpid = do
   (\pid -> terminateProcess pid >> waitForProcess pid) `mapM_` mpid
   callProcess "git" ["checkout", "master"]
   callProcess "git" ["pull"]
+  buildThorough
+
+buildThorough :: IO ProcessHandle
+buildThorough = do
   callProcess "./build" ["setup"]
   callProcess "./build" ["clean"]
-  callProcess "./build" ["build-backend"]
-  callProcess "./build" ["build-frontend"]
+  buildFast
+
+buildFast :: IO ProcessHandle
+buildFast = do
+  callProcess "./build" ["build-backend", "build-frontend"]
   withCurrentDirectory "pkg/frontend" $ callProcess "npm" ["run", "build"]
   withCurrentDirectory "pkg/backend"  $ spawnProcess "stack" ["exec", "--", "refine", "server.conf"]
