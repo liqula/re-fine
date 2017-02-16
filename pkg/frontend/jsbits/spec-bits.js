2bf6f240b3ec968298ef49fe6b0ee331fac47215
Nicole Rauch <info@nicole-rauch.de>
2017-02-16 17:17:24 +0100

    Document and Window must be accessible before React is required.

--- pkg/frontend/jsbits/spec-bits.js
+++ pkg/frontend/jsbits/spec-bits.js
@@ -1,11 +1,11 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var enzyme = require('enzyme');
-
 var jsdom = require('jsdom').jsdom;
 require('jsdom-global')();
 
 var document = jsdom('');
 var window = document.defaultView;
 window.navigator = { userAgent: 'node.js' };
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var enzyme = require('enzyme');
