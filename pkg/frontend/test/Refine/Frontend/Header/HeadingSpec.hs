4c92ff35555b449eeab2a0e50887b0f43e3818d7
Nicole Rauch <info@nicole-rauch.de>
2017-02-25 22:05:26 +0100

    Adapted the tests to the renaming of a component.

--- pkg/frontend/test/Refine/Frontend/Header/HeadingSpec.hs
+++ pkg/frontend/test/Refine/Frontend/Header/HeadingSpec.hs
@@ -42,10 +42,10 @@ import qualified Refine.Frontend.Types as RS
 
 spec :: Spec
 spec = do
-  describe "The mainMenu_ component" $ do
+  describe "The topMenuBar_ component" $ do
     context "not sticky" $ do
       it "renders its elements" $ do
-        wrapper <- shallow (mainMenu_ (MainMenuProps False UserLoggedOut))
+        wrapper <- shallow (topMenuBar_ (TopMenuBarProps False UserLoggedOut))
         lengthOfIO (find wrapper (StringSelector ".c-mainmenu")) `shouldReturn` (1 :: Int)
         lengthOfIO (find wrapper (StringSelector ".c-mainmenu__menu-button")) `shouldReturn` (1 :: Int)
         lengthOfIO (find wrapper (StringSelector ".c-mainmenu__icon-bar")) `shouldReturn` (3 :: Int)
@@ -54,18 +54,18 @@ spec = do
         text label `shouldReturn` "MENU"
 
       it "does not render with sticky css class" $ do
-        wrapper <- shallow (mainMenu_ (MainMenuProps False UserLoggedOut))
+        wrapper <- shallow (topMenuBar_ (TopMenuBarProps False UserLoggedOut))
         label <- find wrapper (StringSelector ".c-mainmenu--toolbar-combined")  -- (it's called combined, though, not sticky)
         lengthOf label `shouldReturn` (0 :: Int)
 
     context "sticky" $ do
       it "does not render the label" $ do
-        wrapper <- shallow (mainMenu_ (MainMenuProps True UserLoggedOut))
+        wrapper <- shallow (topMenuBar_ (TopMenuBarProps True UserLoggedOut))
         label <- find wrapper (StringSelector ".c-mainmenu__menu-button-label")
         lengthOf label `shouldReturn` (0 :: Int)
 
       it "renders with sticky css class" $ do
-        wrapper <- shallow (mainMenu_ (MainMenuProps True UserLoggedOut))
+        wrapper <- shallow (topMenuBar_ (TopMenuBarProps True UserLoggedOut))
         label <- find wrapper (StringSelector ".c-mainmenu--toolbar-combined")  -- (it's called combined, though, not sticky)
         lengthOf label `shouldReturn` (1 :: Int)
 
