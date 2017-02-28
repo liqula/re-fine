f25a98a17bd951ab75d329ca02154f0906e73c8d
Matthias Fischmann <mf@zerobuzz.net>
2017-02-28 09:05:03 +0100

    Remove unproductive property test.
    Doing this on arbitrary states rather than on the empty state and
    arbitrary lists would have been slightly more interesting, but I
    like the run time of the frontend test suite a lot so far, and this
    kinda ruined it.

--- pkg/frontend/test/Refine/Frontend/StoreSpec.hs
+++ pkg/frontend/test/Refine/Frontend/StoreSpec.hs
@@ -26,9 +26,6 @@ module Refine.Frontend.StoreSpec where
 
 import Control.Lens ((^.))
 import Test.Hspec
-import Test.QuickCheck
-import Test.QuickCheck.Monadic
-import Test.QuickCheck.Instances ()
 
 import           React.Flux (transform)
 
@@ -46,13 +43,13 @@ spec :: Spec
 spec = do
   describe "Store" $ do
     describe "transform" $ do
-      it "integrates the loaded document list into the store" . property $
-        \list -> monadicIO . run $ do
-            result <- transform (LoadedDocumentList list) emptyGlobalState
-            result ^. gsVDocList `shouldBe` Just list
+      context "gsVDocList" $ do
+        it "integrates the loaded document list into the store" $ do
+          let list = [ID 3]
+          result <- transform (LoadedDocumentList list) emptyGlobalState
+          result ^. gsVDocList `shouldBe` Just list
 
       context "gsScreenState" $ do
         it "adds the header height to the state" $ do
           newState <- transform (ScreenAction (AddHeaderHeight 64)) emptyGlobalState
           _ssHeaderHeight (_gsScreenState newState) `shouldBe` (64 :: Int)
-
