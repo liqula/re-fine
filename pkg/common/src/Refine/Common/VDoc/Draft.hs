930c2bee6f8118281a96716de224f99ada1d78c3
Matthias Fischmann <mf@zerobuzz.net>
2017-03-08 17:18:10 +0100

    Convert (some of) VDocVersion to RawContent.

--- pkg/common/src/Refine/Common/VDoc/Draft.hs
+++ pkg/common/src/Refine/Common/VDoc/Draft.hs
@@ -23,11 +23,13 @@
 module Refine.Common.VDoc.Draft
 where
 
-import Data.Aeson
-import Data.String.Conversions
-import Data.Tree
-import Text.HTML.Parser
-import Text.HTML.Tree
+import           Data.Aeson
+import           Data.String.Conversions
+import           Data.List ((\\))
+import qualified Data.Text as ST
+import           Data.Tree
+import           Text.HTML.Parser
+import           Text.HTML.Tree
 
 import Refine.Common.Types
 
@@ -62,14 +64,14 @@ type EntityRange = (Int, Int)
 data Entity  -- (we don't need this yet.)
 
 data Style =
-    NoStyle
-  | Header1
+    Header1
   | Header2
   | Header3
   | Bold
   | Italic
   | BulletPoint
   | EnumPoint
+  deriving (Eq)
 
 
 instance ToJSON RawContent where
@@ -98,7 +100,6 @@ instance ToJSON EntityMap where
   toJSON _ = object []
 
 instance ToJSON Style where
-  toJSON NoStyle = "NOSTYLE"
   toJSON Header1 = "HEADER1"
   toJSON Header2 = "HEADER2"
   toJSON Header3 = "HEADER3"
@@ -111,9 +112,38 @@ instance ToJSON Style where
 -- * conversion
 
 vdocVersionToRawContent :: VDocVersion 'HTMLCanonical -> RawContent
-vdocVersionToRawContent (VDocVersion forest) = mkRawContent $ go forest
+vdocVersionToRawContent (VDocVersion forest) = mkRawContent . fmap cons . merge . decons [] . tokensFromForest $ forest
+  where
+    -- TODO: decons is very lenient re. unexpected input, and does not generate all the styles yet.
+    -- TODO: allow for styles on parts of blocks (currently every style change opens a new block).
+    decons :: [Style] -> [Token] -> [([Style], ST)]
+    decons styles (TagOpen n as   : tokens) = decons (styleOn n as styles) tokens
+    decons styles (TagClose n     : tokens) = decons (styleOff n styles) tokens
+    decons styles (ContentText st : tokens) = (styles, st) : decons styles tokens
+    decons styles _                         = []
 
-go :: Forest Token -> [Block ()]
-go [] = []
-go _ = []  -- TODO: i am wondering at this point if we really want to have the html rep.  what about
-           -- always using draft, even for the view mode?
+    styleOn :: TagName -> [Attr] -> [Style] -> [Style]
+    styleOn "h1"     _ = (Header1:)
+    styleOn "h2"     _ = (Header2:)
+    styleOn "h3"     _ = (Header3:)
+    styleOn "strong" _ = (Bold:)
+    styleOn _        _ = id
+
+    styleOff :: TagName -> [Style] -> [Style]
+    styleOff "h1"     = (\\ [Header1])
+    styleOff "h2"     = (\\ [Header2])
+    styleOff "h3"     = (\\ [Header3])
+    styleOff "strong" = (\\ [Bold])
+    styleOff _        = id
+
+    merge :: [([Style], ST)] -> [([Style], ST)]
+    merge = id
+
+    cons :: ([Style], ST) -> Block ()
+    cons (styles, st) = Block st [] ((range,) <$> styles) 0
+      where
+        range = (0, ST.length st - 1)
+
+
+-- TODO: i am wondering at this point if we really want to have the html rep.  what about
+-- always using draft, even for the view mode?
