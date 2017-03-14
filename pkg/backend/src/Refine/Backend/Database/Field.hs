9e288ec1826c0edce6eb96b5a848e27379e4dfd4
Andor Penzes <andor.penzes@gmail.com>
2017-03-14 14:36:28 +0100

    Define JSON field encoding.
    * toPersistJSONValue
    * fromPersistJSONValue

--- pkg/backend/src/Refine/Backend/Database/Field.hs
+++ pkg/backend/src/Refine/Backend/Database/Field.hs
@@ -5,13 +5,14 @@
 module Refine.Backend.Database.Field where
 
 import Control.Monad ((<=<))
-import Data.Aeson (encode, eitherDecode)
+import Data.Aeson
 import Data.Proxy
 import Data.String.Conversions (LBS, ST, cs)
 import Database.Persist
 import Database.Persist.Sql
 
 import Refine.Backend.DocRepo.Core
+import Refine.Common.Types.Access (Role(..))
 import Refine.Common.Types.Chunk
 import Refine.Common.Types.VDoc
 
@@ -51,20 +52,33 @@ instance PersistField EditHandle where
   fromPersistValue              = fmap EditHandle . fromPersistValue
 
 instance PersistFieldSql EditHandle where
-  -- CAUTION: This should be generated, to represent the actual inner type
-  -- of the title
   sqlType _ = sqlType (Proxy :: Proxy ST)
 
+-- * JSON stored values
+
+toPersistJSONValue :: (ToJSON a) => a -> PersistValue
+toPersistJSONValue = toPersistValue . cs @LBS @ST . encode
+
+fromPersistJSONValue :: (FromJSON a) => PersistValue -> Either ST a
+fromPersistJSONValue = (either (Left . cs) Right . eitherDecode . cs) <=< fromPersistValue @ST
+
 instance PersistField ChunkRange where
-  toPersistValue range = toPersistValue . cs @LBS @ST $ encode range
-  fromPersistValue = (either (Left . cs) Right . eitherDecode . cs) <=< fromPersistValue @ST
+  toPersistValue   = toPersistJSONValue
+  fromPersistValue = fromPersistJSONValue
 
 instance PersistFieldSql ChunkRange where
   sqlType _ = sqlType (Proxy :: Proxy ST)
 
 instance PersistField EditKind where
-  toPersistValue kind = toPersistValue . cs @LBS @ST $ encode kind
-  fromPersistValue = (either (Left . cs) Right . eitherDecode . cs) <=< fromPersistValue @ST
+  toPersistValue   = toPersistJSONValue
+  fromPersistValue = fromPersistJSONValue
 
 instance PersistFieldSql EditKind where
+  sqlType _ = sqlType (Proxy :: Proxy ST)
+
+instance PersistField Role where
+  toPersistValue   = toPersistJSONValue
+  fromPersistValue = fromPersistJSONValue
+
+instance PersistFieldSql Role where
   sqlType _ = sqlType (Proxy :: Proxy ST)
