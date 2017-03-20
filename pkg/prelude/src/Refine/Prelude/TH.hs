a5ad88b3c3d4c74eb9c1f9153b45dd3ddd1f2adb
Andor Penzes <andor.penzes@gmail.com>
2017-03-20 23:56:45 +0100

    Support parametric types in makeRefineType.

--- pkg/prelude/src/Refine/Prelude/TH.hs
+++ pkg/prelude/src/Refine/Prelude/TH.hs
@@ -23,12 +23,12 @@ module Refine.Prelude.TH where
 import Control.Lens (makeLenses, makePrisms)
 import Control.DeepSeq (NFData(..))
 import Data.Aeson (FromJSON(..), ToJSON(..))
+import Data.Maybe (listToMaybe)
 import Language.Haskell.TH
 import Generics.SOP
 import Generics.SOP.NFData (grnf)
 
 import Refine.Prelude.Generic
-
 
 
 mkInstanceD :: Cxt -> Type -> [Dec] -> Dec
@@ -38,12 +38,30 @@ mkInstanceD = InstanceD Nothing
 mkInstanceD = InstanceD
 #endif
 
+typeVarName :: TyVarBndr -> Name
+typeVarName (PlainTV name)        = name
+typeVarName (KindedTV name _kind) = name
+
+firstTypeParam :: Dec -> Maybe Name
+firstTypeParam (DataD _ _ vars _ _ _)    = listToMaybe $ map typeVarName vars
+firstTypeParam (NewtypeD _ _ vars _ _ _) = listToMaybe $ map typeVarName vars
+firstTypeParam dec = error $ "firstTypeParam got invalid Dec " ++ show dec
+
+typeOf :: Name -> Q (Type, Maybe Type)
+typeOf t = do
+  (TyConI dec) <- reify t
+  pure $ case firstTypeParam dec of
+    Nothing -> (ConT t, Nothing)
+    Just p  -> (AppT (ConT t) (VarT p), Just (VarT p))
+
 
 makeSOPGeneric :: Name -> Q [Dec]
-makeSOPGeneric t = pure
-  [ mkInstanceD [] (AppT (ConT (''Generic)) (ConT t)) []
-  , mkInstanceD [] (AppT (ConT (''HasDatatypeInfo)) (ConT t)) []
-  ]
+makeSOPGeneric t = do
+  (t', _mtp) <- typeOf t
+  pure
+    [ mkInstanceD [] (AppT (ConT (''Generic)) t') []
+    , mkInstanceD [] (AppT (ConT (''HasDatatypeInfo)) t') []
+    ]
 
 makeJSON :: Name -> Q [Dec]
 makeJSON t = do
@@ -51,10 +69,13 @@ makeJSON t = do
   (VarE gtoJSONDefN)    <- [|Refine.Prelude.Generic.gtoJSONDef|]
   (VarE parseJSONN)     <- [|Data.Aeson.parseJSON|]
   (VarE gparseJSONDefN) <- [|Refine.Prelude.Generic.gparseJSONDef|]
+  let toJSONC   = ConT ''ToJSON
+      fromJSONC = ConT ''FromJSON
+  (t', mtp) <- typeOf t
   pure
-    [ mkInstanceD [] (AppT (ConT (''ToJSON)) (ConT t))
+    [ mkInstanceD (maybe [] (\tp -> [AppT toJSONC tp]) mtp) (AppT toJSONC t')
         [ FunD toJSONN [ Clause [] (NormalB (VarE gtoJSONDefN)) []] ]
-    , mkInstanceD [] (AppT (ConT (''FromJSON)) (ConT t))
+    , mkInstanceD (maybe [] (\tp -> [AppT fromJSONC tp]) mtp) (AppT fromJSONC t')
         [ FunD parseJSONN [ Clause [] (NormalB (VarE gparseJSONDefN)) []] ]
     ]
 
@@ -62,18 +83,20 @@ makeNFData :: Name -> Q [Dec]
 makeNFData t = do
   (VarE rnfN)  <- [|Control.DeepSeq.rnf|]
   (VarE grnfN) <- [|Generics.SOP.NFData.grnf|]
+  let nfdataC = ConT ''NFData
+  (t', mtp) <- typeOf t
   pure
-    [ mkInstanceD [] (AppT (ConT (''NFData)) (ConT t))
+    [ mkInstanceD (maybe [] (\tp -> [AppT nfdataC tp]) mtp) (AppT (ConT (''NFData)) t')
         [ FunD rnfN [ Clause [] (NormalB (VarE grnfN)) []] ]
     ]
 
--- FIXME: Support parametric types
+-- FIXME: Support parametric types, with more than one parameter.
 -- FIXME: rename to makeInstances
 makeRefineType :: Name -> Q [Dec]
 makeRefineType t = do
   l <- makeLenses t
   p <- makePrisms t
-  s <- makeSOPGeneric t
   j <- makeJSON t
   n <- makeNFData t
+  s <- makeSOPGeneric t
   pure $ concat [l, p, s, j, n]
