d6b115d5e7362b76141ee5cfc7c2f52a3586ed4f
Andor Penzes <andor.penzes@gmail.com>
2017-04-01 22:45:30 +0200

    Futurework comment on Allow typeclass.

--- pkg/common/src/Refine/Common/Allow.hs
+++ pkg/common/src/Refine/Common/Allow.hs
@@ -6,6 +6,9 @@ module Refine.Common.Allow where
 import Refine.Common.Types
 
 
+-- FUTUREWORK: Process target can be split up to Group and just
+-- the process data. This abstraction can be extended to
+-- handle other processes like administration group.
 class Allow process target where
   allow
     :: Maybe (ID User)
