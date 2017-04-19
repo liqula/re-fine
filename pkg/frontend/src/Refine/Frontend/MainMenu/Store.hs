3909e05f6d36b3168b2b27acd609eabe46db6017
Matthias Fischmann <mf@zerobuzz.net>
2017-04-19 17:22:37 +0200

    Cleanup.

--- pkg/frontend/src/Refine/Frontend/MainMenu/Store.hs
+++ pkg/frontend/src/Refine/Frontend/MainMenu/Store.hs
@@ -7,23 +7,23 @@ import Refine.Frontend.MainMenu.Types
 import Refine.Frontend.Store.Types (GlobalAction(..))
 
 
--- TODO: Alignment
 mainMenuUpdate :: GlobalAction -> MainMenuState -> MainMenuState
-mainMenuUpdate (MainMenuAction MainMenuActionClose) state = state & mmState .~ MainMenuClosed
+mainMenuUpdate (MainMenuAction MainMenuActionClose) state = state
+  & mmState                    .~ MainMenuClosed
   & mmErrors . mmeLogin        .~ Nothing
   & mmErrors . mmeRegistration .~ Nothing
 
-mainMenuUpdate (MainMenuAction (MainMenuActionOpen tab)) state =
-  state & mmState .~ MainMenuOpen tab
+mainMenuUpdate (MainMenuAction (MainMenuActionOpen tab)) state = state
+  & mmState .~ MainMenuOpen tab
 
-mainMenuUpdate (MainMenuAction (MainMenuActionLoginError e)) state =
-  state & mmErrors . mmeLogin .~ Just e
+mainMenuUpdate (MainMenuAction (MainMenuActionLoginError e)) state = state
+  & mmErrors . mmeLogin .~ Just e
 
 mainMenuUpdate (MainMenuAction (MainMenuActionRegistrationError e)) state = state
   & mmErrors . mmeRegistration .~ Just (cs $ show e)
 
-mainMenuUpdate (MainMenuAction MainMenuActionClearErrors) state =
-  state & mmErrors . mmeLogin        .~ Nothing
-        & mmErrors . mmeRegistration .~ Nothing
+mainMenuUpdate (MainMenuAction MainMenuActionClearErrors) state = state
+  & mmErrors . mmeLogin        .~ Nothing
+  & mmErrors . mmeRegistration .~ Nothing
 
 mainMenuUpdate _ state = state
