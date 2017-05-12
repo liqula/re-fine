40b6be283c68719f1d7e5d267c5b6863ca68db6b
Matthias Fischmann <mf@zerobuzz.net>
2017-05-12 20:49:17 +0200

    Elaborate on hspec-discover issue.

--- pkg/frontend/test/Spec.hs
+++ pkg/frontend/test/Spec.hs
@@ -1,1 +1,6 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover -optF --module-name=Spec #-}
+
+-- if you get errors about calling hspec-discover, check out these links:
+-- - http://zb2/re-fine/re-fine/issues/278
+-- - http://stackoverflow.com/questions/43305277/hspec-discover-executable-sometimes-isnt-found-even-though-installed
+-- - https://github.com/haskell-servant/servant/issues/51#issuecomment-238198742
