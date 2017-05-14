354c65a2cc83e9782991bee2214f7fe6e8658638
Matthias Fischmann <mf@zerobuzz.net>
2017-05-14 15:42:50 +0200

    Make document throw bubble positioning actions.

--- pkg/frontend/src/Refine/Frontend/Document/Document.hs
+++ pkg/frontend/src/Refine/Frontend/Document/Document.hs
@@ -27,40 +27,70 @@ module Refine.Frontend.Document.Document
   ) where
 
 import           Control.Lens ((^?), (^.), (.~), (&), has, view)
+import           Control.Monad (forM_)
 import           Data.Aeson
 import           Data.Aeson.Types (Pair)
 import           Data.String.Conversions
 import qualified Data.Text as ST
 import           GHCJS.Types
 import           React.Flux
+import qualified React.Flux.Outdated as Outdated
 
 import           Refine.Common.Types
 import           Refine.Common.VDoc.Draft
+import           Refine.Frontend.Contribution.Types
 import           Refine.Frontend.Document.FFI
 import           Refine.Frontend.Document.Types
+import           Refine.Frontend.Screen.Calculations
 import           Refine.Frontend.Store
 import           Refine.Frontend.Store.Types
 import           Refine.Frontend.ThirdPartyViews (editor_)
+import           Refine.Frontend.Types
+import           Refine.Frontend.Util
 
 
-document :: View '[DocumentProps]
-document = mkView "Document" $ \props ->
-  article_ [ "id" $= "vdocValue"  -- FIXME: do we still use this?
-           , "className" $= "gr-20 gr-14@desktop editor_wrapper c-article-content"
-           ] $ do
-    let dstate = props ^. dpDocumentState
-    editor_
-      [ "editorState" &= (dstate ^. documentStateVal)
-      , "customStyleMap" &= documentStyleMap (dstate ^? documentStateContent)
-      , setReadOnly (has _DocumentStateView dstate)
-      , onChange $ \evt ->
-          let dstate' :: DocumentState
-              dstate' = dstate & documentStateVal .~ updateEditorState evt
-          in dispatch . DocumentAction . DocumentUpdate $ dstate'
-      ] mempty
+document :: Outdated.ReactView DocumentProps
+document = Outdated.defineLifecycleView "Document" () Outdated.lifecycleConfig
+  { Outdated.lRender = \() props -> liftViewToStateHandler $ do
+      article_ [ "id" $= "vdocValue"  -- FIXME: do we still use this?
+               , "className" $= "gr-20 gr-14@desktop editor_wrapper c-article-content"
+               ] $ do
+        let dstate = props ^. dpDocumentState
+        editor_
+          [ "editorState" &= (dstate ^. documentStateVal)
+          , "customStyleMap" &= documentStyleMap (dstate ^? documentStateContent)
+          , setReadOnly (has _DocumentStateView dstate)
+          , onChange $ \evt ->
+              let dstate' :: DocumentState
+                  dstate' = dstate & documentStateVal .~ updateEditorState evt
+              in dispatch . DocumentAction . DocumentUpdate $ dstate'
+          ] mempty
+
+  , Outdated.lComponentDidMount = Just $ \getPropsAndState _ldom _setState -> do
+      props <- Outdated.lGetProps getPropsAndState
+      ()    <- Outdated.lGetState getPropsAndState  -- (just to show there's nothing there)
+
+      case props ^? dpDocumentState . documentStateContent of
+        Nothing -> pure ()  -- FIXME: do we need to position bubbles in edit mode?  how can we
+                            -- efficiently keep 'RawContent' and 'EditorState' in sync?
+        Just rawContent -> do
+          let marks :: [(ContributionID, MarkSelector, MarkSelector)] = getMarkSelectors rawContent
+
+          -- FIXME: turn 'MarkPosition' into 'MarkPositions' (containing a list instead of just one
+          -- element), and only throw one action per mount (instead of one per mark).
+          forM_ marks $ \(cid, top, bot) -> do
+            topOffset    <- OffsetFromViewportTop  <$> getMarkSelectorBound top
+            bottomOffset <- OffsetFromViewportTop  <$> getMarkSelectorBound bot
+            scrollOffset <- ScrollOffsetOfViewport <$> js_getScrollOffset
+            let markPosition = MarkPosition
+                  { _markPositionTop    = offsetFromDocumentTop topOffset    scrollOffset
+                  , _markPositionBottom = offsetFromDocumentTop bottomOffset scrollOffset
+                  }
+            dispatchAndExec . ContributionAction $ AddMarkPosition cid markPosition
+  }
 
 document_ :: DocumentProps -> ReactElementM eventHandler ()
-document_ !props = view_ document "document_" props
+document_ props = Outdated.view document props mempty
 
 
 -- | in read-only mode, onchange is not fired even on selection.
