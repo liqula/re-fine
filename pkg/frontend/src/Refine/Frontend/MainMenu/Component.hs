9a5a65b0c5bb84ab93df49b2af26e24b0e76e5e5
Matthias Fischmann <mf@zerobuzz.net>
2017-05-25 10:52:24 +0200

    Cleanup (whitespace).

--- pkg/frontend/src/Refine/Frontend/MainMenu/Component.hs
+++ pkg/frontend/src/Refine/Frontend/MainMenu/Component.hs
@@ -41,49 +41,49 @@ import           Refine.Frontend.Util
 
 topMenuBarInMainMenu :: View '[TopMenuBarInMainMenuProps]
 topMenuBarInMainMenu = mkView "TopMenuBarInMainMenu" $ \(TopMenuBarInMainMenuProps _currentTab currentUser) ->
-      div_ ["className" $= "c-mainmenu-content__header"] $ do
-        div_ ["className" $= "gr-2"] $ do
-          ibutton_ $ emptyIbuttonProps "Close" [MainMenuAction MainMenuActionClose]
-            & ibListKey .~ "1"
-            & ibDarkBackground .~ True
-            & ibSize .~ XXLarge
-            & ibLabel .~ mempty
+  div_ ["className" $= "c-mainmenu-content__header"] $ do
+    div_ ["className" $= "gr-2"] $ do
+      ibutton_ $ emptyIbuttonProps "Close" [MainMenuAction MainMenuActionClose]
+        & ibListKey .~ "1"
+        & ibDarkBackground .~ True
+        & ibSize .~ XXLarge
+        & ibLabel .~ mempty
 
-        div_ ["className" $= "gr-20"] $ do
-          ibutton_ $ emptyIbuttonProps "Process" [MainMenuAction $ MainMenuActionOpen MainMenuProcess]
-            & ibListKey .~ "2"
-            & ibDarkBackground .~ True
-            & ibSize .~ XXLarge
-            & ibLabel .~ mempty
+    div_ ["className" $= "gr-20"] $ do
+      ibutton_ $ emptyIbuttonProps "Process" [MainMenuAction $ MainMenuActionOpen MainMenuProcess]
+        & ibListKey .~ "2"
+        & ibDarkBackground .~ True
+        & ibSize .~ XXLarge
+        & ibLabel .~ mempty
 
-          ibutton_ $ emptyIbuttonProps "Group" [MainMenuAction $ MainMenuActionOpen MainMenuGroup]
-            & ibListKey .~ "3"
-            & ibDarkBackground .~ True
-            & ibSize .~ XXLarge
-            & ibLabel .~ mempty
+      ibutton_ $ emptyIbuttonProps "Group" [MainMenuAction $ MainMenuActionOpen MainMenuGroup]
+        & ibListKey .~ "3"
+        & ibDarkBackground .~ True
+        & ibSize .~ XXLarge
+        & ibLabel .~ mempty
 
-          ibutton_ $ emptyIbuttonProps "Help" [MainMenuAction $ MainMenuActionOpen MainMenuHelp]
-            & ibListKey .~ "4"
-            & ibDarkBackground .~ True
-            & ibSize .~ XXLarge
-            & ibLabel .~ mempty
+      ibutton_ $ emptyIbuttonProps "Help" [MainMenuAction $ MainMenuActionOpen MainMenuHelp]
+        & ibListKey .~ "4"
+        & ibDarkBackground .~ True
+        & ibSize .~ XXLarge
+        & ibLabel .~ mempty
 
-          ibutton_ $ emptyIbuttonProps "00_joker" [ShowNotImplementedYet]
-            & ibListKey .~ "5"
-            & ibDarkBackground .~ True
-            & ibSize .~ XXLarge
-            & ibLabel .~ mempty
+      ibutton_ $ emptyIbuttonProps "00_joker" [ShowNotImplementedYet]
+        & ibListKey .~ "5"
+        & ibDarkBackground .~ True
+        & ibSize .~ XXLarge
+        & ibLabel .~ mempty
 
-          -- search
+      -- search
 
-          loginStatusButton_ True currentUser
+      loginStatusButton_ True currentUser
 
-        div_ ["className" $= "gr-2"] $ do
-          ibutton_ $ emptyIbuttonProps "00_joker" [ShowNotImplementedYet]
-            & ibListKey .~ "7"
-            & ibDarkBackground .~ True
-            & ibSize .~ XXLarge
-            & ibLabel .~ mempty
+    div_ ["className" $= "gr-2"] $ do
+      ibutton_ $ emptyIbuttonProps "00_joker" [ShowNotImplementedYet]
+        & ibListKey .~ "7"
+        & ibDarkBackground .~ True
+        & ibSize .~ XXLarge
+        & ibLabel .~ mempty
 
 topMenuBarInMainMenu_ :: TopMenuBarInMainMenuProps -> ReactElementM eventHandler ()
 topMenuBarInMainMenu_ !props = view_ topMenuBarInMainMenu "topMenuBarInMainMenu_" props
@@ -99,19 +99,20 @@ tabStyles =
 
 mainMenu :: View '[MainMenuProps]
 mainMenu = mkView "MainMenu" $ \(MainMenuProps menuTab menuErrors currentUser) -> do
- div_ ["className" $= "row row-align-middle c-mainmenu-content"] $ div_ ["className" $= "grid-wrapper"] $ do
-  topMenuBarInMainMenu_ (TopMenuBarInMainMenuProps menuTab currentUser)
-  div_ [ "className" $= "gr-2" ] $ do
-    pure ()
-  div_ [ "className" $= "gr-20"
-       , "style" @@= tabStyles
-       ] $ do
-    case menuTab of
-      MainMenuProcess                          -> "[MainMenuProcess]"
-      MainMenuGroup                            -> "[MainMenuGroup]"
-      MainMenuHelp                             -> "[MainMenuHelp]"
-      MainMenuLogin MainMenuSubTabLogin        -> loginOrLogout_ currentUser (menuErrors ^. mmeLogin)
-      MainMenuLogin MainMenuSubTabRegistration -> registration_  (menuErrors ^. mmeRegistration)
+  div_ ["className" $= "row row-align-middle c-mainmenu-content"] $ do
+    div_ ["className" $= "grid-wrapper"] $ do
+      topMenuBarInMainMenu_ (TopMenuBarInMainMenuProps menuTab currentUser)
+      div_ [ "className" $= "gr-2" ] $ do
+        pure ()
+      div_ [ "className" $= "gr-20"
+           , "style" @@= tabStyles
+           ] $ do
+        case menuTab of
+          MainMenuProcess                          -> "[MainMenuProcess]"
+          MainMenuGroup                            -> "[MainMenuGroup]"
+          MainMenuHelp                             -> "[MainMenuHelp]"
+          MainMenuLogin MainMenuSubTabLogin        -> loginOrLogout_ currentUser (menuErrors ^. mmeLogin)
+          MainMenuLogin MainMenuSubTabRegistration -> registration_  (menuErrors ^. mmeRegistration)
 
 mainMenu_ :: MainMenuTab -> MainMenuErrors -> CurrentUser -> ReactElementM eventHandler ()
 mainMenu_ mt me cu = view_ mainMenu "mainMenu_" (MainMenuProps mt me cu)
