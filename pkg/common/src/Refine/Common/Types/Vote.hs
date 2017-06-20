68275b1823fb78318462393336e08c35027fe3c8
Matthias Fischmann <mf@zerobuzz.net>
2017-06-20 13:15:51 +0200

    Add votesToCount helper.

--- pkg/common/src/Refine/Common/Types/Vote.hs
+++ pkg/common/src/Refine/Common/Types/Vote.hs
@@ -23,6 +23,8 @@ module Refine.Common.Types.Vote where
 import Refine.Common.Prelude
 
 import GHC.Generics (Generic)
+import qualified Data.Map as Map
+import qualified Data.List as List
 
 import Refine.Common.Types.Prelude (ID, User)
 
@@ -32,6 +34,14 @@ data Vote = Yeay | Nay
 type Votes = Map (ID User) Vote
 
 type VoteCount = Map Vote Int
+
+votesToCount :: Votes -> VoteCount
+votesToCount vs = Map.fromList $ case List.group . sort . Map.elems $ vs of
+  [y@(Yeay : _), n@(Nay : _)] -> [(Yeay, length y), (Nay, length n)]
+  [              n@(Nay : _)] -> [(Yeay, 0),        (Nay, length n)]
+  [y@(Yeay : _)]              -> [(Yeay, length y), (Nay, 0)]
+  []                          -> [(Yeay, 0),        (Nay, 0)]
+  bad                         -> error $ "votesToCount: impossible: " <> show bad
 
 
 makeRefineType ''Vote
