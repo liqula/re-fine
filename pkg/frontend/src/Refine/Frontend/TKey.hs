d89c50403c5a57ce531f9b5291e7920f7606d890
Matthias Fischmann <mf@zerobuzz.net>
2017-07-03 18:02:50 +0200

    Update translation tables.

--- pkg/frontend/src/Refine/Frontend/TKey.hs
+++ pkg/frontend/src/Refine/Frontend/TKey.hs
@@ -1,13 +1,10 @@
-{-# LANGUAGE NoImplicitPrelude          #-}
 module Refine.Frontend.TKey where
-
-import Refine.Frontend.Prelude
 
 import Refine.Common.Types.Translation
 import Data.Text
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
 
-add_a_comment :: HasCallStack => TKey
+add_a_comment :: TKey
 add_a_comment = TKey $ pack "add_a_comment"
 
