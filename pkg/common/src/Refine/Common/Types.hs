5670771867316355815d4f5890a20171f2de0ab3
Peter Divianszky <divipp@gmail.com>
2017-07-28 09:27:50 +0200

    remove empty module

--- pkg/common/src/Refine/Common/Types.hs
+++ pkg/common/src/Refine/Common/Types.hs
@@ -4,7 +4,6 @@ module Refine.Common.Types (module R) where
 import Refine.Common.Types.Core         as R hiding (OTDoc, DocBlock, LineElem)
 import Refine.Common.Types.Comment      as R
 import Refine.Common.Types.Contribution as R
---import Refine.Common.Types.Group        as R
 import Refine.Common.Types.Prelude      as R
 import Refine.Common.Types.Role         as R
 import Refine.Common.Types.Translation  as R
--- pkg/common/src/Refine/Common/Types/Group.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE NoImplicitPrelude          #-}
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies    #-}
-
-module Refine.Common.Types.Group where
