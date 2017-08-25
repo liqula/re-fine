4ab501bfba9c2abd2f1cb38e2fe4dae20ffd65c5
Matthias Fischmann <mf@zerobuzz.net>
2017-08-25 13:06:18 +0200

    Test backend for ws tests (with race conditions).

--- pkg/backend/test/Refine/Backend/WebSocketSpec.hs
+++ pkg/backend/test/Refine/Backend/WebSocketSpec.hs
@@ -8,20 +8,29 @@ import Refine.Backend.Prelude
 import Control.Concurrent
 import Control.Concurrent.Async
 import Data.Set (findMin)
-import qualified Data.Tree as Tree
 import qualified Data.Set as Set
+import qualified Data.Tree as Tree
+import Network.Wai.Handler.Warp
 import Network.WebSockets
 import System.Random
+import System.Timeout
 import Test.Hspec
 
-import Refine.Common.Types as Common
-import Refine.Common.Test.Samples
+import Refine.Backend.App.Access
+import Refine.Backend.App.MigrateDB (initializeDB)
+import Refine.Backend.Config
+import Refine.Backend.Server (backendServer)
+import Refine.Backend.Test.AppServer
+import Refine.Common.Test
+import Refine.Common.Types
+
+{-# ANN module ("HLint: ignore Reduce duplication" :: String) #-}
 
 
 -- * config
 
-verbose :: Bool
-verbose = True
+verbose_ :: Bool
+verbose_ = True
 
 
 -- * primitives
@@ -33,13 +42,47 @@ receiveMessage :: HasCallStack => Connection -> IO ToClient
 receiveMessage conn = receiveData conn <&> fromMaybe (error "websockets json decoding error") . decode
 
 askQuestion :: HasCallStack => Connection -> ToServer -> IO ToClient
-askQuestion conn question = sendMessage conn question >> receiveMessage conn
+askQuestion conn question
+  = fromMaybe (error $ "askQuestion timed out: " <> show question)
+    <$> timeout' (TimespanSecs 1) (sendMessage conn question >> receiveMessage conn)
 
-runWS :: HasCallStack => (Connection -> IO a) -> IO a
-runWS action = do
+timeout' :: Timespan -> IO a -> IO (Maybe a)
+timeout' = timeout . timespanUs
+
+runWS :: HasCallStack => Int -> (Connection -> IO a) -> IO a
+runWS port action = do
   let host :: String = "localhost"
-      port :: Int    = 3000
   runClient host port "/" action
+
+-- | Like @createTestSession' wsBackendCfg@, but runs the 'Application' on a free port, and provides
+-- a WS 'Connection' to it.
+--
+-- FUTUREWORK: a) integrate this with the code in "Refine.Backend.Test.AppServer"; b) multiple
+-- connections for multiple users.
+wsBackend :: (WSBackend -> IO ()) -> IO ()
+wsBackend = wsBackend' (TimespanSecs 3)
+
+wsBackend' :: Timespan -> (WSBackend -> IO ()) -> IO ()
+wsBackend' timespan action = do
+  Just () <- timeout' timespan $ do
+    let cfg = wsBackendCfg
+    createTestSession' cfg $ \(tbe :: TestBackend) -> do
+      testWithApplication (pure . backendServer . view testBackend $ tbe) $ \port -> do
+        runWS port $ \conn -> do
+          runDB tbe . unsafeAsGod $ initializeDB sampleContent
+          action $ WSBackend tbe port conn
+  pure ()
+
+wsBackendCfg :: Config
+wsBackendCfg = testBackendCfg
+        & cfgLogger .~ LogCfg (LogCfgFile testLogfilePath) LogDebug
+        & cfgAllAreGods .~ False
+
+data WSBackend = WSBackend
+  { _testWSBackend :: TestBackend
+  , _testWSPort    :: Int
+  , _testWSConn    :: Connection
+  }
 
 
 -- * combinators
@@ -49,106 +92,30 @@ runWS action = do
 throttle :: IO ()
 throttle = threadDelay =<< randomRIO (1, 1000000)
 
-stresser :: Int -> IO (Async Int)
-stresser rounds = async $ do
-  TCGreeting cid <- runWS $ \conn ->
+stresser :: Int -> Int -> IO (Async Int)
+stresser port rounds = async $ do
+  TCGreeting cid <- runWS port $ \conn ->
     sendMessage conn (TSGreeting Nothing) >> receiveMessage conn
-  forM_ [0..rounds] $ \_ -> runWS $ \conn -> do
-    when verbose $ hPutStrLn stderr ("<" <> show cid <> ">")
+  forM_ [0..rounds] $ \_ -> runWS port $ \conn -> do
+    when verbose_ $ hPutStrLn stderr ("<" <> show cid <> ">")
     sendMessage conn (TSGreeting (Just cid))
     throttle
     TCRestrictKeys [] <- receiveMessage conn
     throttle
     sendMessage conn (TSLogin (Login "nobody" mempty))
     _ <- receiveMessage conn
-    when verbose $ hPutStrLn stderr ("</" <> show cid <> ">")
+    when verbose_ $ hPutStrLn stderr ("</" <> show cid <> ">")
   pure cid
 
-stressers :: Int -> Int -> IO ()
-stressers agents rounds = do
-  as :: [Async Int] <- forM [0..agents] . const $ stresser rounds
+stressers :: Int -> Int -> Int -> IO ()
+stressers port agents rounds = do
+  as :: [Async Int] <- forM [0..agents] . const $ stresser port rounds
   result <- show <$> wait `mapM` as
-  when verbose $ hPutStrLn stderr result
+  when verbose_ $ hPutStrLn stderr result
   length result `shouldNotBe` 0
 
 
 -- * test suite
-
-{- before these tests work, run this somewhere else:
-
-cd pkg/backend/
-stack build --fast && rm -rf .backend-data/ && stack exec refine -- --init sample-content.yaml server.conf && stack exec refine -- server.conf
-
-server.conf:
-
-_cfgPoolSize: 5
-_cfgSessionLength:
-  TimespanHours: 72
-_cfgCsrfSecret: CSRF-SECRET
-_cfgSmtp:
-  _smtpSendmailArgs:
-  - -t
-  _smtpSenderEmail: postmaster@localhost
-  _smtpDefaultRecipient: postmaster@localhost
-  _smtpSendmailPath: /usr/sbin/sendmail
-  _smtpSenderName: Re-fine Notification System
-_cfgFileServeRoot: ../frontend/js-build
-_cfgPoFilesRoot: ./po
-_cfgLogger:
-  _logCfgLevel: LogDebug
-  _logCfgTarget: LogCfgStdOut
-_cfgWarpSettings:
-  _warpSettingsPort: 3000
-  _warpSettingsHost: HostIPv4
-_cfgDBKind:
-  DBOnDisk: ./.backend-data/refine.db
-  # DBInMemory
-_cfgClient:
-  _clientCfgWSHost: localhost
-  _clientCfgWSPort: 3000
-  _clientCfgWSSSL: False
-_cfgWSPingPeriod:
-  TimespanSecs: 14
-_cfgAllAreGods: False
-
-
-sample-content.yaml:
-
-- CliCreateGroup:
-    _createGroupTitle: Universe
-    _createGroupDesc: The group that contains everything
-    _createGroupParents: []
-    _createGroupChildren: []
-- CliCreateGroup:
-    _createGroupTitle: Greek Party
-    _createGroupDesc: Something about ethnics and politics
-    _createGroupParents: []
-    _createGroupChildren: []
-- CliCreateUser:
-  - _cuName: admin
-    _cuPwd: pass
-    _cuMail: admin@localhost
-  - []
-  - - GlobalAdmin
-- CliCreateUser:
-  - _cuName: edna
-    _cuPwd: pass
-    _cuMail: edna@localhost
-  - - - GroupMember
-      - Edna's home group
-    - - GroupMember
-      - Greek Party
-  - []
-- CliCreateUser:
-  - _cuName: joe
-    _cuPwd: pass
-    _cuMail: joe@localhost
-  - - - GroupMember
-      - Joe's home group
-  - []
-
--}
-
 
 spec :: Spec
 spec = do
@@ -156,14 +123,19 @@ spec = do
   specErrors
 
 specStories :: Spec
-specStories = describe "stories" $ do
+specStories = describe "stories" . around wsBackend $ do
   it "works with many concurrent, high-volume sessions" $ do
-    pendingWith "this only works if you run the backend server separately outside the test suite."  -- TODO
-    stressers 100 3
+    \(WSBackend _ port _) -> do
+
+      pendingWith "TODO"
+
+      stressers port 100 3
 
   it "story 1" $ do
-    pendingWith "this only works if you run the backend server separately outside the test suite."  -- TODO
-    runWS $ \conn -> do
+    \(WSBackend _ _ conn) -> do
+
+      pendingWith "TODO"
+
       TCGreeting _ <- askQuestion conn $ TSGreeting Nothing
       respLogin <- askQuestion conn $ TSLogin (Login "admin" "pass")
       show respLogin `shouldContain` "(Right (User {_userMetaID = MetaID {_miID = ID 1, _miMeta = MetaInfo {_metaCreatedBy = Anonymous"
@@ -208,7 +180,7 @@ specStories = describe "stories" $ do
         (disc' ^. discussionTree . to Tree.rootLabel . statementID) (CreateStatement "statement (yes true!)")
       did'' `shouldBe` did
 
-  it "story 2" $ do
+  it "story 2" $ \_ -> do
     pending
 
     -- create two more edits
@@ -248,15 +220,18 @@ make sure we've called all of these at least once:
 
 
 specErrors :: Spec
-specErrors = describe "errors" $ do
-  it "### database lookup failure" $ runWS $ \conn -> do
+specErrors = describe "errors" . around wsBackend $ do
+  it "database lookup failure" $ \(WSBackend _ _ conn) -> do
+
+    pendingWith "TODO"
+
     TCGreeting _ <- askQuestion conn $ TSGreeting Nothing
     respLogin <- askQuestion conn $ TSLogin (Login "admin" "pass")
     show respLogin `shouldContain` "(Right (User {_userMetaID = MetaID {_miID = ID 1, _miMeta = MetaInfo {_metaCreatedBy = Anonymous"
     let noSuchGroup = ID 98691
     TCReset <- askQuestion conn $ TSMissing [CacheKeyGroup noSuchGroup]
-    logShouldContain True ["AppDBError (DBNotFound", show (noSuchGroup ^. unID)]
+    logShouldContain `mapM_` ["AppDBError (DBNotFound", show (noSuchGroup ^. unID)]
 
 
-logShouldContain :: Bool -> [String] -> Expectation
-logShouldContain = undefined
+logShouldContain :: String -> Expectation
+logShouldContain substr = readTestLogfile >>= (`shouldContain` substr)
