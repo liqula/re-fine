b5a2dc3f64e7d057bd4515ee2a121600cdb3a669
daniel <dvde@gmx.net>
2017-09-21 15:01:07 +0200

    Rename CSS classes: groupDetailsProcessesDocument_column1 => groupDetailsProcessesDocument__column1 groupDetailsProcessesDocument_column1-image => groupDetailsProcessesDocument__column1-image groupDetailsProcessesDocument_column2 => groupDetailsProcessesDocument__column2 groupDetailsProcessesDocument_column2-headline => groupDetailsProcessesDocument__column2-headline groupDetailsProcessesDocument_column2-body => groupDetailsProcessesDocument__column2-body groupDetailsProcessesDocument_column3 => groupDetailsProcessesDocument__column3 groupDetailsProcessesDocument_column3-vertical_line => groupDetailsProcessesDocument__column3-vertical-line groupDetailsProcessesDocument_column4 => groupDetailsProcessesDocument__column4 groupDetailsProcessesDocument_column4-item => groupDetailsProcessesDocument__column4-item groupDetailsProcessesDocument_column4-item-icon => groupDetailsProcessesDocument__column4-item-icon groupDetailsProcessesDocument_column4-item-text => groupDetailsProcessesDocument__column4-item-text

--- pkg/frontend/styleguide-new/includes/groupDetailsProcesses-components.php
+++ pkg/frontend/styleguide-new/includes/groupDetailsProcesses-components.php
@@ -2,45 +2,45 @@
 function groupDetailsProcessesDocument() {
 ?>	
 			<div class="groupDetailsProcessesDocument">
-				<div class="groupDetailsProcessesDocument_column1">
-					<img class="groupDetailsProcessesDocument_column1-image" src="testbild.png" alt="">
+				<div class="groupDetailsProcessesDocument__column1">
+					<img class="groupDetailsProcessesDocument__column1-image" src="testbild.png" alt="">
 				</div>
-				<div class="groupDetailsProcessesDocument_column2">
-					<div class="groupDetailsProcessesDocument_column2-headline">
+				<div class="groupDetailsProcessesDocument__column2">
+					<div class="groupDetailsProcessesDocument__column2-headline">
 						Titel des Dokuments ist viel zu lang und so weiter
 					</div>				
-					<div class="groupDetailsProcessesDocument_column2-body">
+					<div class="groupDetailsProcessesDocument__column2-body">
 						Or alis ped ma cum es eium qui si solor autasped et est recae volupta consequi debis abor suntus. Liquidus, nos mo corpore acipidus.Berum unt plia alit voluptat laborio. Ari cumendi aturesciusam quo et quiae. Or alis ped ma cum es eium qui si solor autasped et est recae volupta consequi debis abor suntus. Liquidus, nos mo corpore acipidus. Berum unt plia alit voluptat laborio. Ari cumendi aturesciusam quo et quiae. Or alis ped ma cum es eium qui si solor autasped et est recae volupta consequi debis abor suntus.
 					</div>				
 				</div>
-				<div class="groupDetailsProcessesDocument_column3">
-					<div class="groupDetailsProcessesDocument_column3-vertical_line"></div>
+				<div class="groupDetailsProcessesDocument__column3">
+					<div class="groupDetailsProcessesDocument__column3-vertical-line"></div>
 				</div>
-				<div class="groupDetailsProcessesDocument_column4">
-					<div class="groupDetailsProcessesDocument_column4-item">
-						<div class="groupDetailsProcessesDocument_column4-item-icon">
+				<div class="groupDetailsProcessesDocument__column4">
+					<div class="groupDetailsProcessesDocument__column4-item">
+						<div class="groupDetailsProcessesDocument__column4-item-icon">
 							<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
 						</div>
-						<div class="groupDetailsProcessesDocument_column4-item-text">
+						<div class="groupDetailsProcessesDocument__column4-item-text">
 						3
 						</div>
-						<div class="groupDetailsProcessesDocument_column4-item-icon">
+						<div class="groupDetailsProcessesDocument__column4-item-icon">
 							<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
 						</div>
-						<div class="groupDetailsProcessesDocument_column4-item-text">
+						<div class="groupDetailsProcessesDocument__column4-item-text">
 						9
 						</div>
 					</div>
-					<div class="groupDetailsProcessesDocument_column4-item">
+					<div class="groupDetailsProcessesDocument__column4-item">
 						<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
-						<div class="groupDetailsProcessesDocument_column4-item-text">
+						<div class="groupDetailsProcessesDocument__column4-item-text">
 						3
 						</div>
 						
 					</div>
-					<div class="groupDetailsProcessesDocument_column4-item">
+					<div class="groupDetailsProcessesDocument__column4-item">
 						<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
-						<div class="groupDetailsProcessesDocument_column4-item-text">
+						<div class="groupDetailsProcessesDocument__column4-item-text">
 						3
 						</div>
 					</div>
