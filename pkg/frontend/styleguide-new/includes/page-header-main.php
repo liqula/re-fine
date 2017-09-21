65dc7ff4174cbce3746ec566632b668371d59105
daniel <dvde@gmx.net>
2017-09-21 15:18:32 +0200

    fix ibutton top right

--- pkg/frontend/styleguide-new/includes/page-header-main.php
+++ pkg/frontend/styleguide-new/includes/page-header-main.php
@@ -12,7 +12,7 @@
 
 	<?= icon('ibutton_absolute-topleft', 'svg_close', ['c_fill_blue_night', ' c_fill_note_bubble'], ['ibutton_xxlarge']) ?>
 	<div class="platform-title"></div>
-	<?= icon('ibutton_absolute-topright', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
+	<?= icon('ibutton_absolute-topright', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], ['ibutton_xxlarge']) ?>
 
 	<div class="main-content">
 		<?= commonHeader() ?>
