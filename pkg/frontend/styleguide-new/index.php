25651a8062463d6e064fd0e0919152556feac966
daniel <dvde@gmx.net>
2017-09-22 11:50:29 +0200

    add link to styleguide startpage

--- pkg/frontend/styleguide-new/index.php
+++ pkg/frontend/styleguide-new/index.php
@@ -1,2 +1,3 @@
 <a href="mainMenuGroups.php">mainMenuGroups</a><br>
-<a href="groupDetailsProcesses.php">groupDetailsProcesses</a>
+<a href="groupDetailsProcesses.php">groupDetailsProcesses</a><br>
+<a href="groupDetailsProcesses.php?svg">groupDetailsProcesses mit svg profil bild</a><br>
