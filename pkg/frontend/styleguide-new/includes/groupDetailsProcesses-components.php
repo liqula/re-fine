eba2bc067cc5dff41bac70347424ff341363696a
daniel <dvde@gmx.net>
2017-09-25 12:48:03 +0200

    fix classnames in php

--- pkg/frontend/styleguide-new/includes/groupDetailsProcesses-components.php
+++ pkg/frontend/styleguide-new/includes/groupDetailsProcesses-components.php
@@ -2,45 +2,45 @@
 function groupDetailsDocument() {
 ?>	
 			<div class="groupDetailsDocument">
-				<div class="groupDetailsDocument">
-					<img class="groupDetailsDocument-image" src="testbild.png" alt="">
+				<div class="groupDetailsDocument__column1">
+					<img class="groupDetailsDocument__column1-image" src="testbild.png" alt="">
 				</div>
-				<div class="groupDetailsDocument">
-					<div class="groupDetailsDocument-headline">
+				<div class="groupDetailsDocument__column2">
+					<div class="groupDetailsDocument__column2-headline">
 						Titel des Dokuments ist viel zu lang und so weiter
 					</div>				
-					<div class="groupDetailsDocument-body">
+					<div class="groupDetailsDocument__column2-body">
 						Or alis ped ma cum es eium qui si solor autasped et est recae volupta consequi debis abor suntus. Liquidus, nos mo corpore acipidus.Berum unt plia alit voluptat laborio. Ari cumendi aturesciusam quo et quiae. Or alis ped ma cum es eium qui si solor autasped et est recae volupta consequi debis abor suntus. Liquidus, nos mo corpore acipidus. Berum unt plia alit voluptat laborio. Ari cumendi aturesciusam quo et quiae. Or alis ped ma cum es eium qui si solor autasped et est recae volupta consequi debis abor suntus.
 					</div>				
 				</div>
-				<div class="groupDetailsDocument">
-					<div class="groupDetailsDocument-vertical-line"></div>
+				<div class="groupDetailsDocument__column3">
+					<div class="groupDetailsDocument__column3-vertical-line"></div>
 				</div>
-				<div class="groupDetailsDocument">
-					<div class="groupDetailsDocument-item">
-						<div class="groupDetailsDocument-item-icon">
+				<div class="groupDetailsDocument__column4">
+					<div class="groupDetailsDocument__column4-item">
+						<div class="groupDetailsDocument__column4-item-icon">
 							<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
 						</div>
-						<div class="groupDetailsDocument-item-text">
+						<div class="groupDetailsDocument__column4-item-text">
 						3
 						</div>
-						<div class="groupDetailsDocument-item-icon">
+						<div class="groupDetailsDocument__column4-item-icon">
 							<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
 						</div>
-						<div class="groupDetailsDocument-item-text">
+						<div class="groupDetailsDocument__column4-item-text">
 						9
 						</div>
 					</div>
-					<div class="groupDetailsDocument-item">
+					<div class="groupDetailsDocument__column4-item">
 						<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
-						<div class="groupDetailsDocument-item-text">
+						<div class="groupDetailsDocument__column4-item-text">
 						3
 						</div>
 						
 					</div>
-					<div class="groupDetailsDocument-item">
+					<div class="groupDetailsDocument__column4-item">
 						<?= icon('ibutton_xxlarge', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], []) ?>
-						<div class="groupDetailsDocument-item-text">
+						<div class="groupDetailsDocument__column4-item-text">
 						3
 						</div>
 					</div>
