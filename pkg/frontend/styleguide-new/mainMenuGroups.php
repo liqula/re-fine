ce84af834fe6d5c7ee35d40e01f08faa1c0e46cc
daniel <dvde@gmx.net>
2017-09-25 15:28:17 +0200

    remove common footer, make common header more general

--- pkg/frontend/styleguide-new/mainMenuGroups.php
+++ pkg/frontend/styleguide-new/mainMenuGroups.php
@@ -6,6 +6,16 @@ include "includes/svgs/daniel_test2.php";
 include "includes/svgs/svgs1.php";
 ?>
 <?php include "includes/page-header-main.php"; ?>
+<div class="body-container c_bg_blue_dark">
+
+	<?= icon('ibutton_absolute-topleft', 'svg_close', ['c_fill_blue_night', ' c_fill_note_bubble'], ['ibutton_xxlarge']) ?>
+	<div class="platform-title"></div>
+	<?= icon('ibutton_absolute-topright', 'svgDanielUser', ['c_fill_interaction_yellow', ' c_fill_note_bubble'], ['ibutton_xxlarge']) ?>
+
+	<div class="main-content">
+		<?= commonHeader() ?>
+		<div class="hr-div"></div>
+
 		<div class="mainMenuGroups">
 
 			<?php mainMenuGroupsSmall(example_mainMenuGroupsSmall('c_bg_blue_dawn', 'testbild.png',       NULL,                                                      'Stadtentwicklungspolitik der SPD', 'svgDanielUser', ["c_fill_yes_color", "c_fill_interaction_yellow"],         '12', 'svg_info',     ["c_fill_interaction_yellow", "c_fill_yes_color"], '99', 'svgDanielUser', ["c_fill_yes_color", "c_fill_interaction_yellow"], '77')); ?>
@@ -15,4 +25,5 @@ include "includes/svgs/svgs1.php";
 			<?php mainMenuGroupsSmall(example_mainMenuGroupsSmall('c_bg_blue_dawn', 'testbild.png',       NULL,                                                      'Initiative für eine grüne Stadthsajkdhasjkdhaskjdhasjk', 'svgDanielUser', ["c_fill_yes_color", "c_fill_interaction_yellow"],         '12', 'svg_edit_new',  ["c_fill_yes_color", "c_fill_interaction_yellow", "c_fill_note_bubble"], '99', 'svgDanielUser', ["c_fill_yes_color", "c_fill_interaction_yellow"], '77')); ?>
 
 		</div>			
-<?php include "includes/page-footer-main.php"; ?>
+	</div>
+</div>
