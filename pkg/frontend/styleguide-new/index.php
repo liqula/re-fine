a0c955adb406301c26c2bc40428231ae74a89a85
daniel <dvde@gmx.net>
2017-09-26 14:10:55 +0200

    add forms styleguide

--- pkg/frontend/styleguide-new/index.php
+++ pkg/frontend/styleguide-new/index.php
@@ -3,3 +3,4 @@
 <a href="groupDetailsProcesses.php?svg">groupDetailsProcesses mit svg profil bild</a><br>
 <a href="discussion.php">discussion</a><br>
 <a href="dashboard.php">dashboard</a><br>
+<a href="forms.php">forms</a><br>
