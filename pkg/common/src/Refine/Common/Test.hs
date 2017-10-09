040a0ced962552df3dca88b09065d987e1cc4dff
Matthias Fischmann <mf@zerobuzz.net>
2017-10-09 13:55:15 +0200

    trace-before-after utility functions.

--- pkg/common/src/Refine/Common/Test.hs
+++ pkg/common/src/Refine/Common/Test.hs
@@ -2,7 +2,7 @@
 #include "language_common.hs"
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Refine.Common.Test (module P, passes, failsOn, thisException) where
+module Refine.Common.Test (module P, passes, failsOn, thisException, traceBeforeAfterM, traceBeforeAfter) where
 #include "import_common.hs"
 
 import Test.Hspec as P
@@ -26,3 +26,19 @@ thisException e (SomeException e') = es == es'
   where
     es = show e
     es' = take (length es) $ show e'
+
+
+{-# ANN traceBeforeAfterM ("HLint: ignore Use errorDoNotUseTrace" :: String) #-}
+traceBeforeAfterM :: (Show a, Monad m) => String -> m a -> m a
+traceBeforeAfterM msg act = do
+  () <- trace (">>> before: " <> msg) $ pure ()
+  result <- act
+  () <- trace (">>> after" <> (show . length . show $ result) <> ": " <> msg) $ pure ()
+  pure result
+
+{-# ANN traceBeforeAfter ("HLint: ignore Use errorDoNotUseTrace" :: String) #-}
+traceBeforeAfter :: (Show a) => String -> a -> a
+traceBeforeAfter msg val =
+  trace (">>> before: " <> msg) .
+  trace (">>> after(" <> (show . length . show $ val) <> "): " <> msg) $
+  val
