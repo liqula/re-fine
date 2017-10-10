2bcc59ac054394ff75494d6cd35c3255fbe8ac7a
daniel <dvde@gmx.net>
2017-10-10 12:30:56 +0200

    add topmenubar

--- pkg/frontend/styleguide-new/new/menupage-top.php
+++ pkg/frontend/styleguide-new/new/menupage-top.php
@@ -1,11 +1,40 @@
-	<div class="ibutton-absolute-topleft">
-		<div class="ibutton_xxxlarge">
-			<?php svgDanielUser('c_fill_interaction_yellow', 'c_fill_blue_dawn'); ?>
+<div class="topmenubar" style="display: flex; justify-content: space-between;">
+	
+	<div class="left-column m-t-1 m-l-1 m-r-1 m-b-1">
+		<div class="inner-column-1">
+			<div class="ibutton_xxxlarge">
+				<?php svgDanielUser('c_fill_interaction_yellow', 'c_fill_blue_dawn'); ?>
+			</div>
+		</div>
+		<div class="inner-column-1">
+			<div class="ibutton_xxxlarge">
+				<?php svgDanielUser('c_fill_interaction_yellow', 'c_fill_blue_dawn'); ?>
+			</div>
+		</div>
+		<div class="inner-column-1">
+			<div class="ibutton_xxxlarge">
+				<?php svgDanielUser('c_fill_interaction_yellow', 'c_fill_blue_dawn'); ?>
+			</div>
 		</div>
 	</div>
-	<div class="platform-title"></div>
-	<div class="ibutton-absolute-topright">
-		<div class="ibutton_xxxlarge">
-			<?php svgDanielUser('c_fill_interaction_yellow', 'c_fill_blue_dawn'); ?>
+
+	<div class="platform-title">	
+	</div>
+
+	<div class="right-column m-t-1 m-l-1 m-r-1 m-b-1">
+		<div class="inner-column-1">
+			<div class="ibutton_xxxlarge">
+			</div>
+		</div>
+		<div class="inner-column-1">
+			<div class="ibutton_xxxlarge">
+			</div>
+		</div>
+		<div class="inner-column-1">
+			<div class="ibutton_xxxlarge">
+				<?php svgDanielUser('c_fill_interaction_yellow', 'c_fill_blue_dawn'); ?>
+			</div>
 		</div>
 	</div>
+
+</div>
