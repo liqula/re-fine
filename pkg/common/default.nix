85ee9ffead860f233aa3194c8f400dc1369a0eed
Matthias Fischmann <mf@zerobuzz.net>
2017-10-12 19:52:36 +0200

    cabal2nix.

--- pkg/common/default.nix
+++ pkg/common/default.nix
@@ -1,10 +1,9 @@
 { mkDerivation, aeson, base, basic-sop, blaze-html, blaze-markup
-, checkers, containers, deepseq, fingertree-tf, functor-infix
-, generics-sop, hspec, hspec-checkers, hspec-core
-, hspec-golden-aeson, http-api-data, i18n, json-sop, lens
-, monoid-extras, mtl, natural-transformation, patience, QuickCheck
-, quickcheck-instances, refine-prelude, servant, stdenv
-, string-conversions, text, time, transformers
+, checkers, containers, deepseq, fingertree-tf, generics-sop, hspec
+, hspec-checkers, hspec-core, hspec-golden-aeson, http-api-data
+, i18n, json-sop, lens, monoid-extras, mtl, natural-transformation
+, patience, QuickCheck, quickcheck-instances, refine-prelude
+, servant, stdenv, string-conversions, text, time, transformers
 , unordered-containers, vector, xml-html-qq
 }:
 mkDerivation {
@@ -13,20 +12,18 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     aeson base basic-sop checkers containers deepseq fingertree-tf
-    functor-infix generics-sop hspec hspec-core http-api-data i18n
-    json-sop lens monoid-extras mtl natural-transformation patience
-    QuickCheck quickcheck-instances refine-prelude servant
-    string-conversions text time transformers unordered-containers
-    vector
+    generics-sop hspec hspec-core http-api-data i18n json-sop lens
+    monoid-extras mtl natural-transformation patience QuickCheck
+    quickcheck-instances refine-prelude servant string-conversions text
+    time transformers unordered-containers vector
   ];
   testHaskellDepends = [
     aeson base basic-sop blaze-html blaze-markup checkers containers
-    deepseq fingertree-tf functor-infix generics-sop hspec
-    hspec-checkers hspec-core hspec-golden-aeson http-api-data i18n
-    json-sop lens monoid-extras mtl natural-transformation patience
-    QuickCheck quickcheck-instances refine-prelude servant
-    string-conversions text time transformers unordered-containers
-    vector xml-html-qq
+    deepseq fingertree-tf generics-sop hspec hspec-checkers hspec-core
+    hspec-golden-aeson http-api-data i18n json-sop lens monoid-extras
+    mtl natural-transformation patience QuickCheck quickcheck-instances
+    refine-prelude servant string-conversions text time transformers
+    unordered-containers vector xml-html-qq
   ];
   homepage = "https://github.com/fisx/refine";
   description = "Refine common parts for frontend and backend";
--- pkg/prelude/default.nix
+++ pkg/prelude/default.nix
@@ -1,10 +1,9 @@
 { mkDerivation, aeson, base, basic-sop, bytestring, containers
-, data-default, deepseq, directory, functor-infix, generics-sop
-, hspec, hspec-core, http-api-data, json-sop, lens, monad-control
-, mtl, natural-transformation, process, QuickCheck
-, quickcheck-instances, safe, servant, stdenv, stm
-, string-conversions, template-haskell, text, time, transformers
-, unordered-containers
+, data-default, deepseq, directory, generics-sop, hspec, hspec-core
+, http-api-data, json-sop, lens, monad-control, mtl
+, natural-transformation, process, QuickCheck, quickcheck-instances
+, safe, servant, stdenv, stm, string-conversions, template-haskell
+, text, time, transformers, unordered-containers
 }:
 mkDerivation {
   pname = "refine-prelude";
@@ -13,16 +12,16 @@ mkDerivation {
   enableSeparateDataOutput = true;
   libraryHaskellDepends = [
     aeson base basic-sop bytestring containers data-default deepseq
-    directory functor-infix generics-sop http-api-data json-sop lens
-    monad-control mtl natural-transformation process safe servant stm
+    directory generics-sop http-api-data json-sop lens monad-control
+    mtl natural-transformation process safe servant stm
     string-conversions template-haskell text time transformers
     unordered-containers
   ];
   testHaskellDepends = [
     aeson base basic-sop bytestring containers data-default deepseq
-    directory functor-infix generics-sop hspec hspec-core http-api-data
-    json-sop lens monad-control mtl natural-transformation process
-    QuickCheck quickcheck-instances safe servant stm string-conversions
+    directory generics-sop hspec hspec-core http-api-data json-sop lens
+    monad-control mtl natural-transformation process QuickCheck
+    quickcheck-instances safe servant stm string-conversions
     template-haskell text time transformers unordered-containers
   ];
   license = "AGPL";
